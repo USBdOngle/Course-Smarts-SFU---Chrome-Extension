f7547e0a61f5335d6f97e5a72d17bd3047c32f70
Chris <Chris@DESKTOP-L0UOBR8>
2019-04-27 17:08:54 -0700

    Working on a class that grabs the prof name and and course name from course web page

--- content.js
+++ content.js
@@ -1,1 +1,11 @@
-console.log("hello");
+
+/*
+This allows me to be more modular in how I wrote my content script.
+Currently it does not allow for imports so we can get our content script to inject the scripts we want
+ */
+
+const script = document.createElement('script');
+script.setAttribute("type", "module");
+script.setAttribute("src", chrome.extension.getURL('source/main.js'));
+const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
+head.insertBefore(script, head.lastChild);
