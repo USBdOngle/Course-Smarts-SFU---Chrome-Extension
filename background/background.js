bac554334eaf34c22d0d5a09a31753b83e038b1e
Chris <Chris@DESKTOP-L0UOBR8>
2019-05-02 11:28:48 -0700

    Able to get both courses and profs from database

--- background/background.js
+++ background/background.js
@@ -1,4 +1,3 @@
-
 const config = {
     apiKey: "AIzaSyDgrFkovtvf9JU8q_nnQ3c3YsLQ-ZFriC4",
     authDomain: "course-smarts-sfu.firebaseapp.com",
@@ -13,11 +12,15 @@ firebase.initializeApp(config);
 //sign in user anonymously so they can make requests to firestore db
 firebase.auth().signInAnonymously()
     .catch(error => {
-        console.log(error.id);
+        console.log("Unable to sign in anonymously to Firebase error id: " + error.id);
         //TODO make it so it will retry if there is an error
     });
 
 let dbTest= new dbInterface(firebase.firestore());
 dbTest.getCourse("CMPT 120", () =>{
-    console.log('made it here');
+    console.log('getCourse callback ran');
+});
+
+dbTest.getProfs(["Lisa Craig", "Shit Head", "Paul Saunders"], () =>{
+    console.log("getProfs callback ran");
 });
--- background/database.js
+++ background/database.js
@@ -5,6 +5,7 @@ class dbInterface{
         this._courseRef = firestore.collection("courses");
     }
 
+    //gets document containing the course name and runs passed in callback on that data
     getCourse(courseName, callback){
         this._courseRef.where("course", "==", courseName).get()
             .then(snapshot => {
@@ -15,18 +16,41 @@ class dbInterface{
                     snapshot.docs.map(document => {
                         console.log("Retrieved course: " + courseName + " from database with details...");
                         console.log(document.data());
-                        callback()
+                        callback() //TODO update this so it runs properly when I decide the best callback to use
                     })
                 }
             })
             .catch(error =>{
-                console.log(error.data);
+                console.log("error retrieving course: " + courseName + "error: " + error.data);
                 //TODO maybe add something more intelligent here in the future
             })
     }
 
-    //getProfs(profNames, callback)
+    //profNames should be an Array
+    getProfs(profNames, callback){
+        profNames.forEach(prof => {
+            this._queryProfs(prof, callback)
+        });
+    }
 
-
-
+    //gets document containing the prof's name and runs passed in callback on it
+    _queryProfs(profName, callback){
+        this._profRef.where("name", "==", profName).get()
+            .then(snapshot => {
+                if (snapshot.empty){
+                    console.log("unable to get prof with name: " + profName + " from database");
+                }
+                else{
+                    snapshot.docs.map(document => {
+                        console.log("Retrieved prof: " + profName + " from database with details...");
+                        console.log(document.data());
+                        callback() //TODO update this so it runs properly when I decide the best callback to use
+                    })
+                }
+            })
+            .catch(error =>{
+                console.log("error retrieving prof: " + profName + "error: " + error.data);
+                //TODO maybe add something more intelligent here in the future
+            })
+    }
 }
