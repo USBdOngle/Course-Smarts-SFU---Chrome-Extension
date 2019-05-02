ee35734dcae40b7cb9cf007f9576c8df503de720
Chris <Chris@DESKTOP-L0UOBR8>
2019-05-02 12:08:51 -0700

    changed pageDetails.js so it was no longer a class but two promise returning functions

--- content/main.js
+++ content/main.js
@@ -1,5 +1,14 @@
-import {pageDetails} from './pageDetails.js';
+import * as pageDetails from './pageDetails.js';
 
+let promise = pageDetails.extractCourse();
+promise.then(name => {
+    console.log(name);
+});
 
-let test = new pageDetails();
-console.log(test.data);
+let profPromise = pageDetails.extractProfs();
+profPromise.then(profs => {
+    console.log(profs);
+})
+.catch(error => {
+    console.log(error);
+});
--- content/pageDetails.js
+++ content/pageDetails.js
@@ -1,30 +1,29 @@
-export class pageDetails{
-    constructor(){
-        this._data = {};
-        this._data['course'] = this._extractCourse(window.location.href);
-        this._data['profs'] = this._extractProfs();
-
-    }
-    get data(){
-        return this._data;
-    }
-    _extractCourse(){
+//Extracts course name from URL and returns a promise
+export function extractCourse(){
+    return new Promise((resolve, reject) => {
         let url = window.location.href;
         let mid = url.lastIndexOf('/'); //number portion of course name after this
-        let secondHalf = url.substring(mid+1, url.lastIndexOf('.')).toUpperCase();
-        url = url.substring(0,mid); //remove portion of string we just extracted
-        let firstHalf = url.substring(url.lastIndexOf('/')+1).toUpperCase();
-        return firstHalf + ' ' + secondHalf
-    }
-    _extractProfs() {
+        let secondHalf = url.substring(mid + 1, url.lastIndexOf('.')).toUpperCase();
+        let newUrl = url.substring(0, mid); //remove portion of string we just extracted
+        let firstHalf = newUrl.substring(newUrl.lastIndexOf('/') + 1).toUpperCase();
+        let course = firstHalf + ' ' + secondHalf;
+
+        if (course.length > 1) resolve(course);
+        else reject(new Error("Unable to get course name from: " + url));
+    });
+}
+
+//Extracts an array of all the profs found in the DOM of a course page, returns a promise
+export function extractProfs(){
+    return new Promise((resolve, reject) => {
         let profs = document.getElementsByClassName('main-section');
         let profList = [];
         for (let i = 0; i < profs.length; i++) {
             let potential = profs[i].getElementsByTagName('td')[1].innerText.trim();
 
             let newline = potential.search('\n');
-            if (newline !== -1){
-                potential = potential.substr(0,newline); //sometimes a profs name and a sessional will be put together
+            if (newline !== -1) {
+                potential = potential.substr(0, newline); //sometimes a profs name and a sessional will be put together
             }
 
             //make sure prof name is unique, we don't care about sessional or faculty people
@@ -32,7 +31,9 @@ export class pageDetails{
                 profList.push(potential);
             }
         }
-        return profList;
-    }
+
+        if (profList.length > 0) resolve(profList);
+        else reject(new Error("No profs found in page DOM"));
+    });
 }
 
