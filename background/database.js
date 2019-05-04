c501713c6a6d3bb96430349881076ba4627200b5
Chris <Chris@DESKTOP-L0UOBR8>
2019-05-03 22:24:50 -0700

    able to successfully send queried data from database from background script to content script

--- background/database.js
+++ background/database.js
@@ -1,22 +1,24 @@
-class dbInterface{
+class dbInterface {
     //takes firebase.firestore object
-    constructor(firestore){
+    constructor(firestore) {
         this._profRef = firestore.collection("profs");
         this._courseRef = firestore.collection("courses");
+        this._queriedProfs = []; //will be used to store the profs we get from db
     }
 
     //gets document containing the course name and runs passed in callback on that data
-    getCourse(courseName, callback){
+    getCourse(courseName, callback) {
         this._courseRef.where("course", "==", courseName).get()
-            .catch(error =>{
+            .catch(error => {
                 console.log("error retrieving course: " + courseName + " error: " + error.data);
+                callback(null);
                 //TODO maybe add something more intelligent here in the future
             })
             .then(snapshot => {
-                if (snapshot.empty){
+                if (snapshot.empty) {
                     console.log("unable to get course with name: " + courseName + " from database");
-                }
-                else{
+                    callback(null);
+                } else {
                     snapshot.docs.map(document => {
                         console.log("Retrieved course: " + courseName + " from database with details...");
                         console.log(document.data());
@@ -26,33 +28,43 @@ class dbInterface{
             })
     }
 
-    //profNames should be an Array
-    async getProfs(profNames, callback){
-        let queriedProfs = [];
-        profNames.forEach(prof => {
-            queriedProfs.push(this._queryProfs(prof));
-        });
-        await callback(queriedProfs);
-    }
 
+    //This is probably bad practice to do what I did below, but with my inexperience in javascript
+    //it was the best I could come up with
+    //profNames should be an Array
+    getProfs(profNames, callback) {
+        this._queriedProfs.length  = 0; //clear everything in array
+        this._queryProfs(profNames)
+            .then(() => {
+                callback(this._queriedProfs);
+        })
+            .catch(error => {
+                console.log(error);
+                callback(null);
+            })
+
+    }
+    
     //gets document containing the prof's name and runs passed in callback on it
-    _queryProfs(profName){
-        this._profRef.where("name", "==", profName).get()
-            .then(snapshot => {
-                if (snapshot.empty){
-                    console.log("unable to get prof with name: " + profName + " from database");
-                }
-                else{
-                    snapshot.docs.map(document => {
-                        console.log("Retrieved prof: " + profName + " from database with details...");
-                        console.log(document.data());
-                        return document.data();
-                    })
-                }
-            })
-            .catch(error =>{
-                console.log("error retrieving prof: " + profName + "error: " + error.data);
-                //TODO maybe add something more intelligent here in the future
-            })
+    async _queryProfs(profNames) {
+        for (let i=0; i < profNames.length; i++) {
+            await this._profRef.where("name", "==", profNames[i]).get()
+                .then(snapshot => {
+                    if (snapshot.empty) {
+                        console.log("unable to get prof with name: " + profNames[i] + " from database");
+                    } else {
+                        snapshot.docs.map(document => {
+                            console.log("Retrieved prof: " + profNames[i] + " from database with details...");
+                            console.log(document.data());
+                            this._queriedProfs.push(document.data());
+                        })
+                    }
+                })
+                .catch(error => {
+                    console.log("error retrieving prof: " + prof + "error: " + error.data);
+                    //TODO maybe add something more intelligent here in the future
+                })
+        }
+        if (this._queriedProfs.length === 0) throw new Error("No profs retrieved, nothing to do");
     }
 }
